f810c3bcdc5b89bfff7d12611c226fba8b8426d0
Jordan <jordanshackelford2@gmail.com>
2024-07-09 13:14:50 +0700

    quick commit

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,15 @@
 const { getDefaultConfig } = require('@expo/metro-config');
+const { mergeConfig } = require('@react-native/metro-config');
 
-module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig(__dirname);
-  return {
-    ...defaultConfig,
+const config = async () => {
+  const expoDefaultConfig = await getDefaultConfig(__dirname);
+  
+  return mergeConfig(expoDefaultConfig, {
     server: {
-      ...defaultConfig.server,
-      port: 8082,
+      port: 8082, // Keep your custom port
     },
     // Any additional customizations can be added here
-  };
-})();
+  });
+};
+
+module.exports = config();
